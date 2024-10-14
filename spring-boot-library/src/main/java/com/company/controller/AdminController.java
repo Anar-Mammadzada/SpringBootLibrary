0548924a5d934d23502a5c97140d03cc0322082a
Anar-Mammadzada <anarmammadza@gmail.com>
2024-10-14 21:03:39 +0400

    Add increase, decrease and delete book functionality for administration

--- spring-boot-library/src/main/java/com/company/controller/AdminController.java
+++ spring-boot-library/src/main/java/com/company/controller/AdminController.java
@@ -19,6 +19,26 @@ public class AdminController {
         this.adminService = adminService;
     }
 
+    @PutMapping("/secure/increase/book/quantity")
+    public void increaseBookQuantity(@RequestHeader(value = "Authorization") String token, @RequestParam Long bookId)throws Exception{
+        String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
+        if (admin == null || !admin.equals("admin")){
+            throw new Exception("Administration page only");
+        }
+
+        adminService.increaseBookQuantity(bookId);
+    }
+
+    @PutMapping("/secure/decrease/book/quantity")
+    public void decreaseBookQuantity(@RequestHeader(value = "Authorization") String token, @RequestParam Long bookId)throws Exception{
+        String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
+        if (admin == null || !admin.equals("admin")){
+            throw new Exception("Administration page only");
+        }
+
+        adminService.decreaseBookQuantity(bookId);
+    }
+
     @PostMapping("/secure/add/book")
     public void postBook(@RequestHeader(value = "Authorization") String token, @RequestBody AddBookRequest request) throws Exception{
         String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
@@ -27,5 +47,15 @@ public class AdminController {
         }
 
         adminService.postBook(request);
+    }
+
+    @DeleteMapping("/secure/delete/book")
+    public void deleteBook(@RequestHeader(value = "Authorization") String token, @RequestParam Long bookId) throws Exception{
+        String admin = ExtractJWT.payloadJWTExtraction(token, "\"userType\"");
+        if (admin == null || !admin.equals("admin")){
+            throw new Exception("Administration page only");
+        }
+
+        adminService.deleteBook(bookId);
     }
 }
